1d2054e0c17de50b813295fb16fb74dc9b7b119d
unknown <ayondevs.94b@gmail.com>
2026-05-15 15:27:39 +0530

    added banner footer home page and initial layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-     <h1>Home Page</h1>
+      <Banner></Banner>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,33 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
+        <nav className='flex justify-between items-center bg-white top-0 sticky p-5'>
+            {/* left menu */}
+            <ul className='flex gap-3'>
                 <Link href={'/'}><li>Home</li></Link>
                 <Link href={'/destinations'}><li>Destinations</li></Link>
             </ul>
-{/* logo div */}
-            <div className="">
 
+            {/* logo div */}
+            <div className="">
+                <Image
+                    src={'/assets/Wanderlast.png'}
+                    alt='logo'
+                    height={200}
+                    width={200}>
+
+                </Image>
+            </div>
+
+            {/* right menu */}
+            <div className="">
+                <ul className='flex gap-3'>
+                    <Link href={'/login'}><li>Login</li></Link>
+                    <Link href={'/signup'}><li>Signup</li></Link>
+                </ul>
             </div>
         </nav>
     );
