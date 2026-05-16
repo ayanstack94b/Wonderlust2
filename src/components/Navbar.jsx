3369308eceea027c3482bf4e62cdefbe75b371c9
unknown <ayondevs.94b@gmail.com>
2026-05-16 18:57:26 +0530

    Implementing login page and showing user data on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
+"use client"
+import { authClient } from '@/lib/auth-client';
+import { Avatar, Button } from '@heroui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+
+    const {
+        data: session,
+    } = authClient.useSession()
+
+    const user = session?.user
+    console.log(user);
+
     return (
         <nav className='flex justify-between items-center bg-white top-0 sticky p-5'>
             {/* left menu */}
@@ -19,9 +30,21 @@ const Navbar = () => {
 
             {/* right menu */}
             <div className="">
-                <ul className='flex gap-3'>
-                    <li><Link href={'/login'}>Login</Link></li>
-                    <li><Link href={'/signup'}>Signup</Link></li>
+                <ul className='flex items-center gap-3'>
+                   {user ? (<>
+                   <li>
+                            <Avatar>
+                                <Avatar.Image alt="John Doe" src={user?.image} />
+                                <Avatar.Fallback>{user.name.charAt(0)}</Avatar.Fallback>
+                            </Avatar>
+                   </li>
+                   <li>
+                    <Button className={'rounded-none'} variant='danger'>Logout</Button>
+                   </li>
+                   </>) : <>
+                        <li><Link href={'/login'}>Login</Link></li>
+                        <li><Link href={'/signup'}>Signup</Link></li>
+                   </>}
                 </ul>
             </div>
         </nav>
